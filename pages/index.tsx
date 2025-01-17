92ab32fc4af6bf2981a9eafeb4c2520add6bd394
chuks dozie <chuksdozie48@gmail.com>
2025-01-17 08:04:23 +0100

    Fix: initial index setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import PrimaryButtton from "@/components/buttons/PrimaryButtton";
+import colors from "@/constants/colors";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex bg-gray-400 h-[100vh] items-center justify-center">
-      <div className="bg-gray-500 flex w-[700px] h-[500px] justify-center items-center">
+    <main
+      className={`flex bg-[${colors.white}] h-[100vh] items-center justify-center`}
+    >
+      <div className="bg-gray-500 flex w-[700px] h-[500px] justify-center items-center rounded-md">
         DCC Community
         <PrimaryButtton />
       </div>
