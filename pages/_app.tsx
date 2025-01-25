72df92c9bca5e8dc30cddca43e39c79ffe64f2e7
chuks dozie <chuksdozie48@gmail.com>
2025-01-25 11:12:58 +0100

    Chore: Page Layout Initialized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import SEO from "../next-seo.config";
 import { DefaultSeo } from "next-seo";
+import PageLayout from "@/layout/PageLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
     </>
   );
 }
