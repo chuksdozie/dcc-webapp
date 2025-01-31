37911504d96826cbbe2c76167c5b60c8f4cd828d
chuks dozie <chuksdozie48@gmail.com>
2025-01-31 19:28:02 +0100

    maintenance mode fix

--- layout/PageLayout.tsx
+++ layout/PageLayout.tsx
@@ -3,7 +3,8 @@ import NavBar from "@/components/nav/NavBar";
 import React from "react";
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
-  if (false) return <>{children}</>;
+  const maintenance = process.env.NEXT_PUBLIC_MODE === "maintenance"; //
+  if (maintenance) return <>{children}</>;
   return (
     <div>
       <NavBar />
