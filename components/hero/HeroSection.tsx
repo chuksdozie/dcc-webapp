c218339e64a55064a86751f9f870d01cbce59c35
chuks dozie <chuksdozie48@gmail.com>
2025-02-01 06:56:20 +0100

    feat: hero - Desktop

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div>
-      <p>
+    <div className="flex flex-col items-center justify-center bg-gray-100 py-5 min-h-[600px]">
+      <p className="text-sm font-light text-center w-[30%] text-gray-600">
         A thriving community of tech enthusiasts, developers, and innovators.
         Learn, collaborate, and grow with DCC!
       </p>
-      <p>Building Lives Through Tech</p>
+      <p className="text-6xl font-bold text-center mt-6 text-gray-600">
+        Building Lives Through Tech
+      </p>
     </div>
   );
 };
